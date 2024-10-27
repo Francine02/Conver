abd49a70fa4700789a0f5293cc368252fe2b6fa8
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-27 12:46:16 -0300

    feat: add Hero component with title

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+export function Hero() {
+    return (
+        <div className="text-[#262626] dark:text-[#dadada]">
+            <h1
+                className="text-center pt-24 md:pt-10 font-black text-5xl sm:text-6xl md:text-7xl 2xl:text-9xl">
+                Conver:
+            </h1>
+            <p
+                className="text-center pt-5 font-bold text-base sm:text-lg lg:text-2xl tracking-wide">
+                De um formato a outro, a conversão nunca foi tão fácil!
+            </p>
+        </div>
+    )
+}
