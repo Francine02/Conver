1abce1edbb19fc357443991dac24036b19678e56
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-29 19:50:07 -0300

    feat: add onSelect in prop and map document options dynamically in Dropdown

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -2,18 +2,16 @@ import { OptionsDropdown } from "./OptionsDropdown";
 
 interface DropdownProps {
     open: boolean;
+    onSelect: (option: string) => void;
 }
 
-export function Dropdown({ open }: DropdownProps) {
+export function Dropdown({ open, onSelect }: DropdownProps) {
     return (
-        <div className={`z-10 ${open ? "block" : "hidden"} bg-[#dadada] divide-y divide-slate-900 rounded-md w-40 dark:bg-[#c6c6c6] absolute top-full mt-1 overflow-auto`}>
-            <ul className="py-2 text-sm text-gray-700 dark:text-[#262626]" aria-labelledby="dropdownRightButton">
-                <OptionsDropdown text="PDF" />
-                <OptionsDropdown text="DOC" />
-                <OptionsDropdown text="DOCX" />
-                <OptionsDropdown text="ODT" />
-                <OptionsDropdown text="TXT" />
-                <OptionsDropdown text="RTF" />
+        <div className={`z-10 ${open ? "block" : "hidden"} bg-[#dadada] rounded-md w-40 absolute top-full mt-1 overflow-auto`}>
+            <ul className="py-2 text-sm text-gray-700">
+                {["pdf", "doc", "docx", "odt", "txt", "rtf"].map(option => (
+                    <OptionsDropdown key={option} text={option.toUpperCase()} onSelect={() => onSelect(option)} />
+                ))}
             </ul>
         </div>
     );
