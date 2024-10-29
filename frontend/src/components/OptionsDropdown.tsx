f2b65fb00611af4d09fd31d802d3a617a57dc830
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-29 19:43:41 -0300

    feat: add onSelect functionality in OptionsDropdown component

--- frontend/src/components/OptionsDropdown.tsx
+++ frontend/src/components/OptionsDropdown.tsx
@@ -1,11 +1,12 @@
 interface OptionsDropdownProps {
     text: String;
+    onSelect: () => void;
 }
 
-export function OptionsDropdown({text} : OptionsDropdownProps) {
+export function OptionsDropdown({ text, onSelect }: OptionsDropdownProps) {
     return (
         <li>
-            <a className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{text}</a>
+            <a type="text" onClick={onSelect} className="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{text}</a>
         </li>
     );
 }
