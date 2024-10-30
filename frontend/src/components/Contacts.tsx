d8634c07da99dded19595d87b90c841eaae9a463
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-10-30 11:57:39 -0300

    refactor: replace image URL with local version in assets

--- frontend/src/components/Contacts.tsx
+++ frontend/src/components/Contacts.tsx
@@ -1,3 +1,6 @@
+import gith from "../assets/github.png"
+import linkd from "../assets/linkedin.png"
+
 interface ContactsProps {
     github: string;
     name: string;
@@ -9,13 +12,13 @@ export function Contacts({github, name, linkedin} : ContactsProps) {
         <>
             <li>
                 <a href={github} target="_blank" rel="noopener noreferrer" className="hover:underline flex items-center">
-                    <img src="https://img.icons8.com/?size=100&id=LoL4bFzqmAa0&format=png&color=000000" alt="Github" className="w-8" />
+                    <img src={gith} alt="Github" className="w-8" />
                     - {name}
                 </a>
             </li>
             <li>
                 <a href={linkedin} target="_blank" rel="noopener noreferrer" className="hover:underline flex items-center">
-                    <img src="https://img.icons8.com/?size=100&id=MR3dZdlA53te&format=png&color=000000" alt="Github" className="w-8" />
+                    <img src={linkd} alt="Github" className="w-8" />
 
                     - {name}
                 </a>
